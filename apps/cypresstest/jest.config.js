550eb5d2b9563cf58cd0a9f83ecf39a3b3f6ef4e
Christof Müller <c.mueller@itsmueller.ch>
2020-04-08 14:24:18 +0200

    initial

---
+++ apps/cypresstest/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'cypresstest',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/cypresstest',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
